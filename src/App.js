22e3c1b712eada17e638d3c5b9cea641d12bd625
Witold Wrześniewski <witold.wrzesniewski@poczta.fm>
2025-09-23 19:41:15 +0200

    Add global styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Hero from './components/Hero/Hero'
 import SearchForm from './components/SearchForm/SearchForm'
 import List from './components/List/List.js'
+import './styles/normalize.scss';
+import './styles/global.scss';
 
 const App = () => {
   return (
