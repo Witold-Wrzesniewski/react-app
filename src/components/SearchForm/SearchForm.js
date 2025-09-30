0770571a2debd977499ce33bf76848ae37ca8f32
Witold Wrześniewski <witold.wrzesniewski@poczta.fm>
2025-09-30 17:07:09 +0200

    Add Button component

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,12 +1,13 @@
 import styles from './SearchForm.module.scss';
 import TextInput from '../TextInput/TextInput';
+import Button from '../Button/Button';
 
 const SearchForm = () => {
     return (
         <form className={styles.searchForm}>
             <TextInput placeholder="Search…" />
             <TextInput placeholder="Add new column" />
-            <button className={styles.button}>Search</button>
+            <Button text="Search" />
         </form>
     );
   };
